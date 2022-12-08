e7ea39d247d71796c310c099a61d1e5af3c92bf3
luomu <xxxx@qq.com>
2022-12-08 14:47:49 +0800

    弹窗禁止手动关闭

--- index.php
+++ index.php
@@ -696,6 +696,7 @@ if ($ret['code'] != 1) {
                 // '<span style="color:#f00">请正确填写否则无法到账</span><br>' +
                 '<input id="pay_username" placeholder="请输入付款人姓名" style="border: 1px solid #e6e6e6;width: ' + lay_input_with + ';" type="text" class="layui-layer-input" >'
             , btn: '确定'
+            ,closeBtn : 0
             , shadeClose: false
             , yes: function (index) {
                 var url = "{:url('updateOrderPayUsername')}"
