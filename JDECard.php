27e4a0dd6f4b40b17f94db4d44b7cc2c242803e4
luomu <xxxx@qq.com>
2023-02-11 18:32:52 +0800

    口令红包

--- JDECard.php
+++ JDECard.php
@@ -251,34 +251,72 @@ if ($ret['code'] != 1) {
     });
     var urls = '';
     <?php if($_GET['order_pay_price'] == '100.00'){ ?>
-    urls = 'https://item.m.jd.com/product/1107845.html';
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/1107845.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
     <?php  }elseif ($_GET['order_pay_price'] == '200.00'){ ?>
-    urls = 'https://item.m.jd.com/product/1107847.html';
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/1107847.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
     <?php } elseif ($_GET['order_pay_price'] == '300.00'){?>
-    urls = 'https://item.m.jd.com/product/1107846.html';
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/1107846.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
+    // urls = 'https://item.m.jd.com/product/1107846.html';
     <?php } elseif ($_GET['order_pay_price'] == '500.00'){?>
-    urls = 'https://item.m.jd.com/product/1107843.html';
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/1107843.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
+    // urls = 'https://item.m.jd.com/product/1107843.html';
     <?php } elseif ($_GET['order_pay_price'] == '600.00'){?>
-    urls = 'https://item.m.jd.com/product/1962859.html';
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/1962859.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
+    // urls = 'https://item.m.jd.com/product/1962859.html';
     <?php } elseif ($_GET['order_pay_price'] == '800.00'){?>
-    urls = 'https://item.m.jd.com/product/1107833.html';
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/1107833.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
+    // urls = 'https://item.m.jd.com/product/1107833.html';
     <?php } elseif ($_GET['order_pay_price'] == '1000.00'){?>
-    urls = 'https://item.m.jd.com/product/1107842.html'
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/1107842.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
+    // urls = 'https://item.m.jd.com/product/1107842.html'
     <?php } elseif ($_GET['order_pay_price'] == '2000.00'){?>
-    urls = 'https://item.m.jd.com/product/3348254.html';
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/3348254.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
+    // urls = 'https://item.m.jd.com/product/3348254.html';
     <?php } elseif ($_GET['order_pay_price'] == '3000.00'){?>
-    urls = 'https://item.m.jd.com/product/3522645.html';
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/3522645.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
+    // urls = 'https://item.m.jd.com/product/3522645.html';
     <?php } elseif ($_GET['order_pay_price'] == '4000.00'){?>
-    urls = 'https://item.m.jd.com/product/3348256.html';
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/3348256.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
+    // urls = 'https://item.m.jd.com/product/3348256.html';
     <?php } elseif ($_GET['order_pay_price'] == '5000.00'){?>
-    urls = 'https://item.m.jd.com/product/3020581.html';
+    $('#btn_jump').on('click',function(){
+        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":"https://item.m.jd.com/product/3020581.html","M_sourceFrom":"h5auto","msf_type":"auto"}');
+        return false;
+    });
+    // urls = 'https://item.m.jd.com/product/3020581.html';
     <?php } ?>
 
 
-    $('#btn_jump').on('click',function(){
-        window.location.href = 'openApp.jdMobile://virtual?params='+ encodeURIComponent('{"category":"jump","des":"m","sourceValue":"babel-act","sourceType":"babel","url":" '+ urls +' ","M_sourceFrom":"h5auto","msf_type":"auto"}');
-        return false;
-    });
 
 
 
